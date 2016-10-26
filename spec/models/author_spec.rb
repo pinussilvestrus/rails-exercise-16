ea86f4307549952f50f4633db3b08131e3e58d8e
Niklas Kiefer <niklas.kiefer@student.hpi.de>
2016-10-26 15:51:00 +0200

    add name property for author model, closes #16

---
+++ spec/models/author_spec.rb
@@ -0,0 +1,11 @@
+require 'rails_helper'
+
+RSpec.describe Author, type: :model do
+  before :each do
+    @author = FactoryGirl.create(:author)
+  end
+
+  it 'name returns the full name' do
+    expect(@author.name).to eq(@author.first_name + ' ' + @author.last_name)
+  end
+end
