ee1d039023e46bac4b1891e7e65bf42b205e3a77
Niklas Kiefer <niklas.kiefer@student.hpi.de>
2016-10-29 10:30:38 +0200

    create paper post, closes #34

--- app/controllers/papers_controller.rb
+++ app/controllers/papers_controller.rb
@@ -1,4 +1,25 @@
 class PapersController < ApplicationController
   def new
+    @paper = Paper.new
   end
+
+  def create
+    @paper = Paper.new(paper_params)
+
+    if @paper.save
+      # redirect_to @author, notice: 'Author was successfully created.'
+      render :new
+    else
+      render :new
+    end
+  end
+
+  private
+    def set_paper
+      @paper = Paper.find(params[:id])
+    end
+
+    def paper_params
+      params.require(:paper).permit(:title, :venue, :year)
+    end
 end
