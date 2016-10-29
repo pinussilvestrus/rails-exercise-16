4d2fc43e50dead36076d51280c4e334a2303d522
Niklas Kiefer <niklas.kiefer@student.hpi.de>
2016-10-29 10:06:36 +0200

    validate paper mode, closes #30

--- spec/features/papers_spec.rb
+++ spec/features/papers_spec.rb
@@ -7,7 +7,7 @@ describe "Papers features", :type => :feature do
     expect(page).to have_text("Creating new paper")
   end
 
-  it "Paper should have titlw, venue, and year" do
+  it "Paper should have title, venue, and year" do
     @paper = Paper.new(title: "COMPUTING MACHINERY AND INTELLIGENCE", venue: "Mind 49: 433-460", year: 1950)
     expect(@paper).not_to be_nil
     expect(@paper.year).equal?(1950)
