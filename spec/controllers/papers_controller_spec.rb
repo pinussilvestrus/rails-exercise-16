46727b806ccad7203d28617d5d82c3ac520b3f09
Niklas Kiefer <niklas.kiefer@student.hpi.de>
2016-11-08 16:40:51 +0100

    add missing tests, failing test for #46

--- spec/controllers/papers_controller_spec.rb
+++ spec/controllers/papers_controller_spec.rb
@@ -1,5 +1,27 @@
 require 'rails_helper'
 
-RSpec.describe PapersController, type: :controller do
+RSpec.describe PapersController, type: :feature do
+  it 'should render' do
+    visit new_paper_path
+    expect(page).to have_text("Creating new paper")
+  end
 
+  it 'should allow to select 5 authors from 5 separate drop downs' do
+    paper = create(:paper)
+
+    visit edit_paper_path(id: paper.id)
+
+    expect(page).to have_field("Author 1")
+    expect(page).to have_field("Author 2")
+    expect(page).to have_field("Author 3")
+    expect(page).to have_field("Author 4")
+    expect(page).to have_field("Author 5")
+  end
+
+  it 'should list the authors' do
+    paper = create(:paper)
+
+    visit papers_path + "/" + paper.id.to_s
+    expect(page).to have_text("Alan Turing")
+  end
 end
